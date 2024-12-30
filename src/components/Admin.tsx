b9c1e0f99064a7f1fb253f599f4a7f3cbbbfbe58
Mingk42 <xoals123456t@gmail.com>
2024-12-30 18:58:47 +0900

    권한 분기 처리

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import Visualize from "./Visualize";
+import {loadSession} from "../scripts/common";
 
 
 function Admin() {
 
     const navigate = useNavigate();
+    const [isAdmin, setIsAdmin] = useState<boolean>(loadSession("userType")==="1");
 
-    return (
+
+    useEffect(() => {
+        if(!isAdmin){
+            alert("접근권한이 없습니다.")
+            navigate("/")
+        }
+    }, []);
+
+    return isAdmin?(
         <div id="adminContainer" style={{textAlign: "center"}}>
             <h1>Dashboard</h1>
             <button className={"btn btn-warning"} onClick={()=>{navigate(-1)}}  style={{position: "relative", left: "30vw", top: "-10px"}} >뒤로가기🔙 </button>
@@ -15,7 +25,7 @@ function Admin() {
                 <Visualize />
             </div>
         </div>
-    );
+    ):<></>;
 }
 
 export default Admin;
